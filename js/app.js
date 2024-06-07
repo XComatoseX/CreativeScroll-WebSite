77f99056126fb207d3f75ab33eb9bfefee00a79c
XComatoseX <denismanko2006@gmail.com>
2024-06-08 00:48:04 +0300

    ⚡ Update smooth scroll / hero section

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content',
-  smooth: 1.5,
-  effects: true,
-})
+if (ScrollTrigger.isTouch !==1 ) {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true,
+  })
+
+  gsap.fromTo('.hero', { opacity: 1}, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.hero',
+      start: 'center',
+      end: '820',
+      scrub: true
+    }
+  })
+}
