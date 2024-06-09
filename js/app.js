06a23f060f4fea4407a32c9b10e160c5b522f71f
XComatoseX <denismanko2006@gmail.com>
2024-06-10 00:02:13 +0300

    ⚡ Add animation for portfolio item

--- js/app.js
+++ js/app.js
@@ -17,4 +17,33 @@ if (ScrollTrigger.isTouch !==1 ) {
       scrub: true
     }
   })
+
+  let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+  itemsL.forEach(item => {
+    gsap.fromTo(item, {x: -50, opacity: 0}, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        start: `-850`,
+        end: `-100`,
+        scrub: true
+      }
+    })
+  });
+
+  let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+  itemsR.forEach(item => {
+    gsap.fromTo(item, {x: 50, opacity: 0}, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        start: `-850`,
+        end: `-100`,
+        scrub: true
+      }
+    })
+  });
+
 }
